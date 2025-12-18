105a4fdcae0bf1e44287c2043bfad1e9c7dcb9fd
Rezuanul Islam Fahim <rifahim98@gmail.com>
2025-12-18 18:49:07 +0600

    Wrap section heading in a container for improved layout consistency

--- resources/views/components/section-heading.blade.php
+++ resources/views/components/section-heading.blade.php
@@ -1,1 +1,4 @@
-<h3 class="font-bold text-xl">{{ $slot }}</h3>
+<div class="inline-flex gap-x-3 items-center">
+    <span class="w-3 h-3 inline-block bg-white"></span>
+    <h3 class="font-bold text-xl">{{ $slot }}</h3>
+</div>
