7f3adaee76838cce881437ecec59d09d656a29a9
Rezuanul Islam Fahim <rifahim98@gmail.com>
2025-12-19 22:27:39 +0600

    Refactor TagSeeder to associate tags with jobs during seeding

--- database/seeders/TagSeeder.php
+++ database/seeders/TagSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Job;
 use App\Models\Tag;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -13,6 +14,12 @@ class TagSeeder extends Seeder
      */
     public function run(): void
     {
-        Tag::factory(50)->create();
+        $tags = Tag::factory(50)->create();
+
+        Job::all()->each(function (Job $job) use ($tags) {
+            $job->tags()->attach(
+                $tags->random(rand(1, 4))
+            );
+        });
     }
 }
