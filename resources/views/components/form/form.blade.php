c824b86e7eafc93ffc8af33694e1de41227d0df3
Rezuanul Islam Fahim <rifahim98@gmail.com>
2025-12-20 14:34:24 +0600

    Fix form method handling to default to GET and ensure CSRF protection is applied correctly

--- resources/views/components/form/form.blade.php
+++ resources/views/components/form/form.blade.php
@@ -1,10 +1,12 @@
-@props(['method'])
-
 <form
     {{ $attributes([
         'class' => 'flex flex-col gap-y-6 w-2xl mx-auto mb-20',
         'action' => '',
-        'method' => $method,
+        'method' => 'GET',
     ]) }}>
+    @if ($attributes->get('method', 'GET') !== 'GET')
+        @csrf
+        @method($attributes->get('GET'))
+    @endif
     {{ $slot }}
 </form>
