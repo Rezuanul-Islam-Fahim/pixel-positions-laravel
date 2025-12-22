0d073610bfc2d7f2e62d2ed5c4b635b033ee2f4c
Rezuanul Islam Fahim <rifahim98@gmail.com>
2025-12-22 21:42:15 +0600

    Fix spacing in results and tags views: update margin class for consistency

--- resources/views/tags.blade.php
+++ resources/views/tags.blade.php
@@ -1,6 +1,6 @@
 <x-layout>
     <x-general-heading>Results</x-general-heading>
-    <div class="mt-6 space-y-6">
+    <div class="my-6 space-y-6">
         @foreach ($jobs as $job)
             <x-job-card-wide :job="$job" />
         @endforeach
